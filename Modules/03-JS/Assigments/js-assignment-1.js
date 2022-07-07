d18add2edb410aa41c9c0a5e2844c9e54082e913
leinadnat <38252926+leinadnat@users.noreply.github.com>
2022-07-07 17:52:17 +0800

    Delete Modules/03-JS directory

--- Modules/03-JS/Assigments/js-assignment-1.js
+++
@@ -1,9 +0,0 @@
-function sum(numbersArray){  
-    let sum = 0;
-    for (let i = 0; i < numbersArray.length; i++) {
-        sum = sum + numbersArray[i];
-    }
-    console.log(sum);
-}
-sum([1, 2, 3, 4]);
-sum([-3, 5, 19, -6]);
--- Modules/03-JS/Assigments/js-assignment-4.js
+++
@@ -1,9 +0,0 @@
-function tetra(n){  
-    return (n * (n + 1) * (n + 2)) / 6;
-}
-console.log(tetra(2))
-console.log(tetra(5))
-console.log(tetra(6))
-console.log(tetra(1))
-console.log(tetra(3))
-console.log(tetra(4))
