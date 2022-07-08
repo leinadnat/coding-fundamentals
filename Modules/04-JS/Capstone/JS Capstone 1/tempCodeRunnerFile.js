7cfe756ec8d990043efdbe43102c3f168e0d6dbd
leinadnat <38252926+leinadnat@users.noreply.github.com>
2022-07-08 21:18:49 +0800

    Add files via upload

---
+++ Modules/04-JS/Capstone/JS Capstone 1/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+    console.log(missingLetter("abcdefghjklmno"));
+    console.log(missingLetter("stvwx"));
+    console.log(missingLetter("bcdf"));
+    console.log(missingLetter("abcdefghijklmnopqrstuvwxyz"));
